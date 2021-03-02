1d9e6142bb104a6d099e653122f51013176172aa
Ross <conover.ross@gmail.com>
2021-03-01 20:41:49 -0600

    refactor and JS Docs

--- JS/mobile-swipe.js
+++ JS/mobile-swipe.js
@@ -1,3 +1,13 @@
+/**
+ * TOUCH SWIPE
+ * ----------------------------------------------------------------------
+ * A very low-weight Class that creates a Swipe Event Handler on an HTML Element
+ * ----------------------------------------------------------------------
+ * @param {Node}      elem            Element that listens for the Swipe Functionality
+ * @param {Node}      slideElem       Element that slides when the Event is Triggered
+ * @param {Function}  effectCallback  Function passed in that handles what happens when a swipe occurs
+ * ----------------------------------------------------------------------
+ */
 class TOUCHSWIPE{
   constructor(elem, slideElem, effectCallback){
     this._elem = elem;                        // The element to enable touchswipe on
@@ -12,77 +22,138 @@ class TOUCHSWIPE{
     this.buildMobileSwipe();
   }
 
+  /**
+   * BUILD MOBILE SWIPE
+   * ----------------------------------------------------------------------
+   * Sets up the Mobile Swipe Events (touchstart, touchmove, and touchend)
+   * ----------------------------------------------------------------------
+   */
   buildMobileSwipe(){
     this.elem.addEventListener("touchstart", this.touchStartHandle.bind(this) );
     this.elem.addEventListener("touchmove", this.touchMoveHandle.bind(this) );
     this.elem.addEventListener("touchend",this.touchEndHandle.bind(this) );
   }
 
+  /**
+   * TOUCH START HANDLE
+   * ----------------------------------------------------------------------
+   * Handles the effects of the Touch Start Event
+   * ----------------------------------------------------------------------
+   * @param {Event} e Event object 
+   * ----------------------------------------------------------------------
+   */
   touchStartHandle(e){
     if(debug) debug.debugLog("EVENT [touchstart] :: Start point = "+e.touches[0].clientX+" , "+e.touches[0].clientY);
+
+    // Clear existing Deltas and Set the Starting Point
     this.moveDelta = [0,0];
     this.startCoordinates = [e.touches[0].clientX,e.touches[0].clientY];
   }
 
+  /**
+   * TOUCH MOVE HANDLE
+   * ----------------------------------------------------------------------
+   * Handles the effects of the Touch Move Event
+   * ----------------------------------------------------------------------
+   * @param {Event} e Event object 
+   * ----------------------------------------------------------------------
+   */
   touchMoveHandle(e){
-    this.moveHappened = true;
+    this.moveHappened = true; // While true, a moving has happened (prevents a Click from counting as a Move on second interaction)
+
+    // Get the current coordinates and set the Deltas based on difference to Start Points
     this.currCoordinates = [e.touches[0].clientX , e.touches[0].clientY];
     this.moveDelta = this.calculateDelta(this.startCoordinates, this.currCoordinates);
+    
+    // If the Delta is past a certain point, animate the Slide Elem slightly for visual feedback
+    // TODO - Allow for more varieties of effect, not just subtle slide
     if(Math.abs(this.moveDelta[0]) >= 20) this.slide(this.moveDelta);
   }
 
+  /**
+   * TOUCH END HANDLE
+   * ----------------------------------------------------------------------
+   * Handles the effects of the Touch End Event
+   * ----------------------------------------------------------------------
+   * @param {Event} e Event object 
+   * ----------------------------------------------------------------------
+   */
   touchEndHandle(e){
-    let previousClassList = this.slideElem.classList;
     if(debug) debug.debugLog("EVENT [touchend] :: Delta = "+this.moveDelta);
-    if(this.moveHappened){ // This check has to be here to prevent a "click" as counting as a move
-      if(Math.abs(this.moveDelta[0]) >= 50 ){ // The move window, how long it takes before it counts as a "swipe"
+
+    // If the Delta is past a certain number, check which direction from the start the end coordinates are, then send the direction to the Callback Function
+    if(this.moveHappened){  // This check has to be here to prevent a "click" as counting as a move
+      if(Math.abs(this.moveDelta[0]) >= 50 ){ // The move window, how long it takes before it counts as a "swipe" | TODO - Set this value through Param
         if(this.moveDelta[0] > 0){
           this.effectCallback({direction: 'left'});
         } else{
           this.effectCallback({direction: 'right'});
         }
-        this.slideElem.style.transform = "translateX(0px)";
-      } else{
-        this.slideElem.style.transform = "translateX(0px)";
+      } else{ // If you don't meet the delta requirement to trigger the event, just send no direction to the callback
+        this.effectCallback({direction: 'none'});
       }
-      this.moveHappened = false;
+      this.moveHappened = false;  // Reset the Move Happened check to prevent "Click swiping"
     }
   }
 
+  /**
+   * CALCULATE DELTA
+   * ----------------------------------------------------------------------
+   * Takes Start and End Coordinates and returns the differences between them
+   * ----------------------------------------------------------------------
+   * @param {Array} start The X and Y position at the start of the Touch Event
+   * @param {Array} end   The X and Y position at the end of the Touch Event\
+   * @returns An Array of the Deltas between the Start and End points
+   * ----------------------------------------------------------------------
+   */
   calculateDelta(start, end){
-    let delta = [end[0] - start[0], end[1] - start[1]];
-    return delta;
+    return [end[0] - start[0], end[1] - start[1]];
   }
 
+  /**
+   * SLIDE
+   * ----------------------------------------------------------------------
+   * Allows the Slide Element to Slightly Move when dragging
+   * TODO - Allow Y movement as well
+   * ----------------------------------------------------------------------
+   * @param {Array} delta Array of Deltas between the Start and End points
+   * ----------------------------------------------------------------------
+   */
   slide(delta){
     if(Math.abs(delta[0]) <=50 )this.slideElem.style.transform = `translateX(${Math.ceil(delta[0]/2)}px)`;
   }
 
   
   /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-  ::::::::::::::::::::  GETTERS                                                                ::::::::::::::::::::
+  ::::::::::::::::::::                                                                         ::::::::::::::::::::
+  ::::::::::::::::::::  GETTERS & SETTERS                                                      ::::::::::::::::::::
+  ::::::::::::::::::::                                                                         ::::::::::::::::::::
   :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
   
   get elem(){ return this._elem }
-  get slideElem(){ return this._slideElem }
-  get startCoordinates(){ return this._startCoordinates }
-  get currCoordinates(){ return this._currCoordinates }
-  get moveDelta(){ return this._moveDelta }
-  get startX(){ return this._startCoordinates[0] }
-  get startY(){ return this._startCoordinates[1] }
-  get currX(){ return this._currCoordinates[0] }
-  get currY(){ return this._currCoordinates[1] }
-  get moveHappened(){ return this._moveHappened }
-  get effectCallback(){ return this._effectCallback }
-
   
-  /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-  ::::::::::::::::::::  SETTERS                                                                ::::::::::::::::::::
-  :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
-
-  set startCoordinates( coords ) { this._startCoordinates = coords }
-  set currCoordinates( coords ) { this._currCoordinates = coords }
-  set moveDelta( delta ) { this._moveDelta = delta }
+  get slideElem(){ return this._slideElem }
   set slideElem( newElem ) { this._slideElem = newElem }
+  
+  get startCoordinates(){ return this._startCoordinates }
+  set startCoordinates( coords ) { this._startCoordinates = coords }
+
+  get currCoordinates(){ return this._currCoordinates }
+  set currCoordinates( coords ) { this._currCoordinates = coords }
+  
+  get moveDelta(){ return this._moveDelta }
+  set moveDelta( delta ) { this._moveDelta = delta }
+  
+  get startX(){ return this._startCoordinates[0] }
+  
+  get startY(){ return this._startCoordinates[1] }
+  
+  get currX(){ return this._currCoordinates[0] }
+  
+  get currY(){ return this._currCoordinates[1] }
+  
+  get moveHappened(){ return this._moveHappened }
   set moveHappened( bool ) { this._moveHappened = bool }
+  
+  get effectCallback(){ return this._effectCallback }
 }
--- JS/svg-stage.js
+++ JS/svg-stage.js
@@ -1,30 +1,43 @@
+/**
+ * SVG STAGE
+ * ----------------------------------------------------------------------
+ * This is a rather specific JS Object. It's used on my KRILL MEAT site,
+ * to handle drawing different SVGs on a "Stage"
+ * ----------------------------------------------------------------------
+ * @param {Array} doodleNames Array of Strings of the Names of each SVG
+ * @param {Node}  stageElem   Element that SVGs are drawn onto
+ * @param {Node}  labelElem   Element that works as a wrapper with a state/label to control styling
+ * ----------------------------------------------------------------------
+ */
 class SVGSTAGE{
   constructor(doodleNames, stageElem, labelElem){
-    this._doodleNames = doodleNames;  // The list of doodle names
-    this._currentSlideIndex = 0;      // The current doodle's index number
-    this._stageElem = stageElem;      // The element the SVGs are drawn onto
-    this._labelElm = labelElem;       // The element that the labels get put on to style things as you want
-    this._doodleElem = this.buildDoodleElem();
+    this._doodleNames = doodleNames;            // List of doodle names
+    this._currentSlideIndex = 0;                // Current doodle's index number
+    this._stageElem = stageElem;                // Element the SVGs are drawn onto
+    this._labelElm = labelElem;                 // Element that the labels get put on to style things as you want
+    this._doodleElem = this.buildDoodleElem();  // Element that holds the Doodles (SVGs)
 
     this.init();
   }
 
   /** INITIALIZE
-   * ------------------------------------------------------------
+   * ----------------------------------------------------------------------
    * The inital Function that runs after the constructor
-   * ------------------------------------------------------------
+   * ----------------------------------------------------------------------
    */
   init(){
     if(debug && debug.debugOn) debug.debugLog("Enabling SVG Stage...");
+    
+    // Append the Doodle Element to the Stage and set the Doodle to the first one in the list
     this.stageElem.appendChild(this.doodleElem);
     this.switchSvg(this.doodleNames[0]);
   }
 
   /**
    * BUILD DOODLE ELEM
-   * ------------------------------------------------------------
+   * ----------------------------------------------------------------------
    * Builds the initial Doodle Element for the page
-   * ------------------------------------------------------------
+   * ----------------------------------------------------------------------
    */
   buildDoodleElem(){
     let doodleElem = document.createElement("div");
@@ -37,44 +50,56 @@ class SVGSTAGE{
 
   /**
    * EVENT CALLBACK
-   * ------------------------------------------------------------
-   * Callback function - Takes in object and sets things up
-   * ------------------------------------------------------------
-   * @param {Object} callbackParams
-   * ------------------------------------------------------------
+   * ----------------------------------------------------------------------
+   * Callback function - Allows Event-based JS to control the switch function
+   * ----------------------------------------------------------------------
+   * @param {Object} callbackParams An object that contains various settings from the event handler
+   * ----------------------------------------------------------------------
    */
   eventCallback(callbackParams){
+    // Expect callbackParams to include 'direction' of 'left', 'right', or 'none
     let newDoodleIndex = 0;
     if(callbackParams.direction === "left"){
       newDoodleIndex = this.currentSlideIndex === 0 ? this.doodleNames.length-1 : this.currentSlideIndex-1;
     } else if(callbackParams.direction === "right"){
       newDoodleIndex = this.currentSlideIndex === this.doodleNames.length-1 ? 0 : this.currentSlideIndex+1;
     }
+    this.doodleElem.style.transform = "translateX(0px)"; // No matter what, reset the position of the Doodle Element
     this.switchSvg(this.doodleNames[newDoodleIndex]);
   }
 
   /**
    * SWITCH SVG
-   * ------------------------------------------------------------
+   * ----------------------------------------------------------------------
    * Switches out the SVG in the draw zone
-   * ------------------------------------------------------------
+   * ----------------------------------------------------------------------
+   * @param {String} doodleName The name of the Doodle from the Doodle Names List
+   * ----------------------------------------------------------------------
    */
   switchSvg(doodleName){
-    let obj = this;
-    document.body.dataset.current = doodleName;
+    document.body.dataset.current = doodleName; // Set the Body State to the correct Doodle
+    
+    // Reset Doodle Elem to the new SVG
     let doodleElem = document.querySelector(".doodle");
         doodleElem.dataset.current = doodleName;
         doodleElem.innerHTML = `<svg viewBox='0 0 1000 1000'>${svgData[doodleName]}</svg>`;
     
     this.currentSlideIndex = this.doodleNames.indexOf(doodleName);
 
+    // Change the Mobile Selector Dots to show the correctly selected Doodle
     let mobileSelector = document.querySelector(".doodle-selector .mobile-selector");
-
-    mobileSelector.querySelector("button[data-status='selected']").dataset.status = "";
-    mobileSelector.querySelectorAll("button")[this.currentSlideIndex].dataset.status = "selected";
+        mobileSelector.querySelector("button[data-status='selected']").dataset.status = "";
+        mobileSelector.querySelectorAll("button")[this.currentSlideIndex].dataset.status = "selected";
 
     this.stageElem.appendChild(doodleElem);
   }
+
+
+  /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+  ::::::::::::::::::::                                                                         ::::::::::::::::::::
+  ::::::::::::::::::::  GETTERS & SETTERS                                                      ::::::::::::::::::::
+  ::::::::::::::::::::                                                                         ::::::::::::::::::::
+  :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
 
   get doodleNames(){ return this._doodleNames }
 
